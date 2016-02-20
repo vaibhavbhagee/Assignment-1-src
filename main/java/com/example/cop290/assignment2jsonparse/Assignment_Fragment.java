4e2fe8d286d7d55471d6f687cdbad6552aba19e1
Shreyan <shreyan.gupta96@gmail.com>
2016-02-20 16:16:16 +0530

    Course Main Page Layout Added

---
+++ main/java/com/example/cop290/assignment2jsonparse/Assignment_Fragment.java
@@ -0,0 +1,30 @@
+package com.example.cop290.assignment2jsonparse;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.cop290.assignment2jsonparse.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Assignment_Fragment extends Fragment {
+
+
+    public Assignment_Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_assignment_, container, false);
+    }
+
+}
