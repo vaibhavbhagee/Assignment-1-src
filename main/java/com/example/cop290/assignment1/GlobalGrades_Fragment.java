cf5cae547583be89843a7af93d58d2cf47f51677
Vaibhav Bhagee <vaibhavbhagee@gmail.com>
2016-02-22 12:58:46 +0530

    All grades and all Notifications populated

--- main/java/com/example/cop290/assignment1/GlobalGrades_Fragment.java
+++ main/java/com/example/cop290/assignment1/GlobalGrades_Fragment.java
@@ -32,33 +32,41 @@ public class GlobalGrades_Fragment extends Fragment {
     String send_request(){
 
         // Send request to server and return string reply
-
-        return "bla bla";
+        String st = "{\"courses\": [{\"code\": \"cop290\", \"name\": \"Design Practices in Computer Science\", \"description\": \"Design Practices in Computer Science.\", \"credits\": 3, \"id\": 1, \"l_t_p\": \"0-0-6\"}, {\"code\": \"cop290\", \"name\": \"Design Practices in Computer Science\", \"description\": \"Design Practices in Computer Science.\", \"credits\": 3, \"id\": 1, \"l_t_p\": \"0-0-6\"}, {\"code\": \"cop290\", \"name\": \"Design Practices in Computer Science\", \"description\": \"Design Practices in Computer Science.\", \"credits\": 3, \"id\": 1, \"l_t_p\": \"0-0-6\"}], \"grades\": [{\"weightage\": 10.0, \"user_id\": 4, \"name\": \"Assignment 1\", \"out_of\": 15.0, \"registered_course_id\": 1, \"score\": 15.0, \"id\": 10}, {\"weightage\": 15.0, \"user_id\": 4, \"name\": \"Assignment 2\", \"out_of\": 20.0, \"registered_course_id\": 1, \"score\": 20.0, \"id\": 11}, {\"weightage\": 25.0, \"user_id\": 4, \"name\": \"Minor 1\", \"out_of\": 30.0, \"registered_course_id\": 1, \"score\": 15.0, \"id\": 12}]}";
+        return st;
     }
 
     void populateListView(View view) {
 
+        ArrayList<GradesRowObject> gl=new ArrayList<GradesRowObject>();
         try {
             ParseAllGradesJSON p = new ParseAllGradesJSON(send_request());
+//            gl = new ArrayList<GradesRowObject>();
+            for (int i = 0; i <p.courses.length; i++)
+            {
+                gl.add(new GradesRowObject(p.courses[i].code,p.grades[i].name,p.grades[i].score,p.grades[i].weightage,
+                        p.grades[i].out_of));
+            }
         }
         catch(Exception e){
             e.printStackTrace();
         }
-        UserAdapter adapter = new UserAdapter(getActivity(), new ArrayList<ParseAllGradesJSON>());
+
+        UserAdapter adapter = new UserAdapter(getActivity(), gl);
         ListView listView = (ListView) view.findViewById(R.id.listView);
         listView.setAdapter(adapter);
 
     }
 
 
-    public class UserAdapter extends ArrayAdapter<ParseAllGradesJSON> {
-        public UserAdapter(Context context, ArrayList<ParseAllGradesJSON> users) {
+    public class UserAdapter extends ArrayAdapter<GradesRowObject> {
+        public UserAdapter(Context context, ArrayList<GradesRowObject> users) {
             super(context, 0, users);
         }
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            ParseAllGradesJSON user = getItem(position);
+            GradesRowObject item = getItem(position);
             if (convertView == null) {
                 convertView = LayoutInflater.from(getContext()).inflate(R.layout.format_global_grades, parent, false);
             }
@@ -70,9 +78,35 @@ public class GlobalGrades_Fragment extends Fragment {
             TextView absolute_marks = (TextView) convertView.findViewById(R.id.absolute_marks);
 
             //SET ALL THE PARAMETERS HERE
+            slno.setText((position+1)+"");
+            course.setText(item.course+"");
+            grade_item.setText(item.grade_item+"");
+            score.setText(item.score+"");
+            weight.setText(item.weight+"");
+            absolute_marks.setText(item.absolute_marks+"");
 
             return convertView;
         }
     }
 
+    public class GradesRowObject
+    {
+        public String course;
+        public String grade_item;
+        public double score;
+        public double weight;
+        public double absolute_marks;
+
+        public GradesRowObject(String course,String grade_item,double score, double weight,double absolute_marks)
+        {
+            this.course = course;
+            this.grade_item = grade_item;
+            this.score = score;
+            this.weight = weight;
+            this.absolute_marks = absolute_marks;
+        }
+    }
+
 }
+
+
--- main/java/com/example/cop290/assignment1/Notification_Fragment.java
+++ main/java/com/example/cop290/assignment1/Notification_Fragment.java
@@ -6,12 +6,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 
 /**
@@ -39,35 +43,68 @@ public class Notification_Fragment extends Fragment {
 
     void populateListView(View view) {
 
-        ArrayList<ParseNotificationsJSON> test_array = new ArrayList<ParseNotificationsJSON>();
+        ArrayList<NotificationsRowObject> gl=new ArrayList<NotificationsRowObject>();
+        try {
+            ParseNotificationsJSON p = new ParseNotificationsJSON(send_request());
+//            gl = new ArrayList<GradesRowObject>();
+            for (int i = 0; i <p.notifications.length; i++)
+            {
+                DateFormat formatter = new SimpleDateFormat("YYYY-MM-DD HH:mm:ss");
+                Date date = (Date)formatter.parse(p.notifications[i].created_at);
+                Date d = new Date();
+                long getmills = d.getTime()-date.getTime();
+                long hrs = getmills/3600000;
+                long hr_residue = getmills%3600000;
+                long min = hr_residue/60000;
 
-        UserAdapter adapter = new UserAdapter(getActivity(), test_array);
+                gl.add(new NotificationsRowObject(p.notifications[i].description,hrs+" hours, "+min+" minutes ago"));
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+        UserAdapter adapter = new UserAdapter(getActivity(), gl);
 
         ListView listView = (ListView) view.findViewById(R.id.listView);
         listView.setAdapter(adapter);
     }
 
 
-    public class UserAdapter extends ArrayAdapter<ParseNotificationsJSON> {
-        public UserAdapter(Context context, ArrayList<ParseNotificationsJSON> items) {
+    public class UserAdapter extends ArrayAdapter<NotificationsRowObject> {
+        public UserAdapter(Context context, ArrayList<NotificationsRowObject> items) {
             super(context, 0, items);
         }
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
 
-            ParseNotificationsJSON user = getItem(position);
+            NotificationsRowObject user = getItem(position);
             if (convertView == null) {
                 convertView = LayoutInflater.from(getContext()).inflate(R.layout.format_notification, parent, false);
             }
             LinearLayout layout = (LinearLayout) convertView.findViewById((R.id.layout));
             TextView slno = (TextView) convertView.findViewById(R.id.slno);
-            TextView notification = (TextView) convertView.findViewById(R.id.notification);
+            WebView notification = (WebView) convertView.findViewById(R.id.notification);
             TextView time = (TextView) convertView.findViewById(R.id.time);
 
             // RENDER THE INFORMATION IN THE RESPECTIVE FIELDS
+            slno.setText((position+1)+"");
+            notification.loadDataWithBaseURL("",user.notification,"text/html","UTF-8","");
+            time.setText(user.time);
 
             return convertView;
+        }
+    }
+
+    public class NotificationsRowObject
+    {
+        public String notification;
+        public String time;
+
+        public NotificationsRowObject(String notification,String time) {
+            this.notification = notification;
+            this.time = time;
         }
     }
 }
