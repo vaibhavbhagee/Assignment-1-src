a6d2d4b2091c81c3984ae150549b23bc52025199
Vaibhav Bhagee <vaibhavbhagee@gmail.com>
2016-02-22 15:48:04 +0530

    Assignment page population added

--- main/java/com/example/cop290/assignment1/Assignment_Fragment.java
+++ main/java/com/example/cop290/assignment1/Assignment_Fragment.java
@@ -29,40 +29,66 @@ public class Assignment_Fragment extends Fragment {
         return view;
     }
 
+    String send_request(){
+
+        // Send request to server and return string reply
+
+        return "bla bla";
+    }
+
     void populateListView(View view) {
 
+        ArrayList<AssignmentsRowObject> gl=new ArrayList<AssignmentsRowObject>();
         try {
-            ParseAllGradesJSON p = new ParseAllGradesJSON("I don't know what to do\nI don't know what to do");
+            ParseCourseAssignmentsJSON p = new ParseCourseAssignmentsJSON(send_request());
+            for (int i = 0; i <p.assignments.length; i++)
+            {
+                gl.add(new AssignmentsRowObject(p.assignments[i].name,p.assignments[i].deadline));
+            }
         }
         catch(Exception e){
             e.printStackTrace();
         }
-        UserAdapter adapter = new UserAdapter(getActivity(), new ArrayList<ParseAllGradesJSON>());
+        UserAdapter adapter = new UserAdapter(getActivity(), gl);
         ListView listView = (ListView) view.findViewById(R.id.listView);
         listView.setAdapter(adapter);
 
     }
 
 
-    public class UserAdapter extends ArrayAdapter<ParseAllGradesJSON> {
-        public UserAdapter(Context context, ArrayList<ParseAllGradesJSON> users) {
+    public class UserAdapter extends ArrayAdapter<AssignmentsRowObject> {
+        public UserAdapter(Context context, ArrayList<AssignmentsRowObject> users) {
             super(context, 0, users);
         }
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            ParseAllGradesJSON user = getItem(position);
+            AssignmentsRowObject user = getItem(position);
             if (convertView == null) {
                 convertView = LayoutInflater.from(getContext()).inflate(R.layout.format_assignment, parent, false);
             }
             TextView slno = (TextView) convertView.findViewById(R.id.slno);
-            TextView course = (TextView) convertView.findViewById(R.id.name);
-            TextView grade_item = (TextView) convertView.findViewById(R.id.time);
+            TextView name = (TextView) convertView.findViewById(R.id.name);
+            TextView time = (TextView) convertView.findViewById(R.id.time);
 
             //SET ALL THE PARAMETERS HERE
+
+            slno.setText((position+1)+"");
+            name.setText(user.name);
+            time.setText(user.time);
 
             return convertView;
         }
     }
 
+    public  class AssignmentsRowObject
+    {
+        public String name;
+        public String time;
+
+        public AssignmentsRowObject(String name, String time) {
+            this.name = name;
+            this.time = time;
+        }
+    }
 }
